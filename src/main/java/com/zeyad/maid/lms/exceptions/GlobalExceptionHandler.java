02afd2f3a744fba87d704e4d0515f2e9d45cc801
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-04-08 21:01:11 +0200

    handling validtion exception

--- src/main/java/com/zeyad/maid/lms/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/zeyad/maid/lms/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.zeyad.maid.lms.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +24,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = {ResourceExistedException.class})
     protected ResponseEntity<?> handleConflict(ResourceExistedException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : result.getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+
     }
     @ExceptionHandler(value = {HttpStatusCodeException.class})
     protected ResponseEntity<?> handleConflict(HttpStatusCodeException ex) {
