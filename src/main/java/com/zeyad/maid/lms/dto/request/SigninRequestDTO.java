82bb1b3f51cbbe8edf291b8fbd853f66d3fdbb24
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-04-08 21:00:51 +0200

    adding validation over Siginin Request

--- src/main/java/com/zeyad/maid/lms/dto/request/SigninRequestDTO.java
+++ src/main/java/com/zeyad/maid/lms/dto/request/SigninRequestDTO.java
@@ -1,12 +1,18 @@
 package com.zeyad.maid.lms.dto.request;
 
+import jakarta.validation.GroupSequence;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class SigninRequestDTO {
-
+    @NotBlank(message = "Username must not be empty")
+    @Size(min = 5, message = "Username must be at least 5 characters long")
     private String username;
+    @NotBlank(message = "password must not be empty")
+    @Size(min = 5, message = "password must be at least 8 characters long")
     private String password;
 }
