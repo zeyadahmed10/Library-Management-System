b2c5e6ac8eb6c1dcde4b1e7560ec9f22e5a93d3b
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-04-08 17:46:50 +0200

    adding RestTemplate bean at LibraryManagmentSystemApplication

--- src/main/java/com/zeyad/maid/lms/LibraryManagementSystemApplication.java
+++ src/main/java/com/zeyad/maid/lms/LibraryManagementSystemApplication.java
@@ -2,6 +2,8 @@ package com.zeyad.maid.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
@@ -9,5 +11,8 @@ public class LibraryManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
